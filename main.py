346fe14ba47404de3c0e264e80a5635c78ea230c
root <root@LocCool>
2023-03-20 15:20:08 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from ultralytics import YOLO
+import cv2
+import gc
+import time
+
+async def ClearMemory():
+    while True:
+        gc.collect()
+        time.sleep(1)
+
+def main():
+    model = YOLO('yolov8n.pt')
+    ClearMemory()
+    for results in model.track(source="traffic.mp4", show=True, stream=True):
+        frame = results.orig_img
+
+        cv2.imshow('yolov8', frame)
+
+        if (cv2.waitKey(30)==27):
+            break
+
+
+if __name__ == '__main__':
+    main()
